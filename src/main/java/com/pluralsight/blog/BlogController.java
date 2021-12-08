d499a2ceea19889b06cfa53904241041b7c442d6
Robert Popa <robert.popa87@gmail.com>
2021-12-08 14:09:00 +0100

    Step 5
    Display a Post Details Page

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -4,6 +4,7 @@ import com.pluralsight.blog.data.PostRepository;
 import com.pluralsight.blog.model.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -18,10 +19,17 @@ public class BlogController {
     }
 
     @RequestMapping("/")
-    public String listPosts(ModelMap parameterMap){
+    public String listPosts(ModelMap modelMap){
         List<Post> allPosts = postRepository.getAllPosts();
-        parameterMap.put("title", "Blog Post 1");
-        parameterMap.put("posts", allPosts);
+        modelMap.put("title", "Blog Post 1");
+        modelMap.put("posts", allPosts);
         return "home";
+    }
+
+    @RequestMapping("/post/{id}")
+    public String postDetails(@PathVariable Long id, ModelMap modelMap){
+        Post post = postRepository.findById(id);
+        modelMap.put("post", post);
+        return "post-details";
     }
 }
